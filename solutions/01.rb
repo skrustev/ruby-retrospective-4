54b0fb516d0d2ed4a5546030f49f7b9a43b1eea6
skrustev <striker18@abv.bg>
2015-01-18 16:01:48 +0200

    Proper style for case use in task 01

--- solutions/01.rb
+++ solutions/01.rb
@@ -8,8 +8,8 @@ end
 
 def series(type, index)
   case type
-    when 'fibonacci' then sequence(1, 1, index)
-    when 'lucas' then sequence(2, 1, index)
-    when 'summed' then sequence(1, 1, index) + sequence(2, 1, index)
+  when 'fibonacci' then sequence(1, 1, index)
+  when 'lucas' then sequence(2, 1, index)
+  when 'summed' then sequence(1, 1, index) + sequence(2, 1, index)
   end
 end
